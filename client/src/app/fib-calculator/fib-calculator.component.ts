8b7bdb3e9a5fa348d2b9bdc411f059479734ee97
Prashit Prakhar <prashitprakhar@yahoo.com>
2021-02-09 01:14:30 +0530

    default values for properties

--- client/src/app/fib-calculator/fib-calculator.component.ts
+++ client/src/app/fib-calculator/fib-calculator.component.ts
@@ -9,10 +9,11 @@ import { FiboService } from '../services/fibo.service';
 export class FibCalculatorComponent implements OnInit {
 
   public enteredInput;
-  public seenIndicies: any[] = [1, 2, 3];
+  // public seenIndicies: any[] = [1, 2, 3];
+  public seenIndicies: any[] = [];
   public allCalculatedValues: any[] = [
-    {index: 5, value: 8},
-    {index: 6, value: 13}
+    // {index: 5, value: 8},
+    // {index: 6, value: 13}
   ];
 
   constructor(private fiboService: FiboService) { }
